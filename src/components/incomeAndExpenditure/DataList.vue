0873c9149e10b83e05c67cfd30cf621eea118309
zhouze <zhouze0688@dingtalk.com>
2025-11-24 18:24:27 +0800

    feat 收入类型 和 支出类型

--- src/components/incomeAndExpenditure/DataList.vue
+++ src/components/incomeAndExpenditure/DataList.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-table :columns="columns" :data-source="data" :scroll="{ x: 1300, y: 1000 }">
+  <a-table :columns="props.columns" :data-source="data" :scroll="{ x: 1300, y: 1000 }">
     <template #bodyCell="{ column }">
       <template v-if="column.key === 'operation'">
         <a>action</a>
@@ -14,27 +14,9 @@ import type { TableColumnsType } from 'ant-design-vue';
 
 const props = defineProps<{
   listFunc: () => Promise<DataItem[]>,
-  deleteFunc: (id: number) => Promise<void> }
->()
-
-const columns: TableColumnsType = [
-  { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name', fixed: 'left' },
-  { title: 'Age', width: 100, dataIndex: 'age', key: 'age', fixed: 'left' },
-  { title: 'Column 1', dataIndex: 'address', key: '1' },
-  { title: 'Column 2', dataIndex: 'address', key: '2' },
-  { title: 'Column 3', dataIndex: 'address', key: '3' },
-  { title: 'Column 4', dataIndex: 'address', key: '4' },
-  { title: 'Column 5', dataIndex: 'address', key: '5' },
-  { title: 'Column 6', dataIndex: 'address', key: '6' },
-  { title: 'Column 7', dataIndex: 'address', key: '7' },
-  { title: 'Column 8', dataIndex: 'address', key: '8' },
-  {
-    title: 'Action',
-    key: 'operation',
-    fixed: 'right',
-    width: 100,
-  },
-];
+  deleteFunc: (id: number) => Promise<void>,
+  columns: TableColumnsType,
+}>()
 
 interface DataItem {
   key: string;
